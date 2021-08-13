6ad4af0bd78b7de8b5b519227d868bf9c6b9b4ee
Douglas Jansey <douglas.jansey@gmail.com>
2021-08-13 20:39:17 -0300

    comida

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32; 
+let food = {
+    x:Math.floor(Math.random() * 15 + 1) * box,
+    y:Math.floor(Math.random() * 15 + 1) * box
+}
 let snake = [];
 snake[0] = {x: 8 * box, y: 8 * box}
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -14,9 +19,44 @@ function criandocobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-  
+function comida(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+document.addEventListener('keydown', update);
+function update(event){
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+function iniciar(){
 
-criarBG();
-criandocobrinha();
+    if(snake[0].x > 16 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;;''
+    if(snake[0].y > 16 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;;
+    
 
-alert("OLÁ")
+    criarBG();
+    criandocobrinha();
+    comida();
+
+    let snakex = snake[0].x;
+    let snakey = snake[0].y;
+    if (direction == "right") {snakex += box}
+    if (direction == "left"){snakex -= box}
+    if(direction == "up"){snakey -= box}
+    if(direction == "down"){snakey += box}
+    snake.pop();
+    let newhead = {
+        x: snakex,
+        y: snakey
+    }
+    snake.unshift(newhead);
+
+}
+let jogo = setInterval(iniciar, 100);
+
+
+
